39963d1e2ee6edccbda2db72442821f8e65a1915
rohit <surverohita32@gmail.com>
2026-01-07 23:45:38 +0530

    next greater element

---
+++ src/com/dsa/stacks/NextGreaterElement.java
@@ -0,0 +1,31 @@
+package com.dsa.stacks;
+
+import java.util.*;
+
+public class NextGreaterElement {
+	
+	public static int[] nextGreaterElement(int[] nums) {
+		int n=nums.length;
+		int[] result=new int[n];
+		Arrays.fill(result, -1);
+		Stack<Integer> stack=new Stack<>();
+		
+		for(int i=0;i<n;i++) {
+			while(!stack.isEmpty() && nums[i]>nums[stack.peek()]) {
+				int index=stack.pop();
+				result[index]=nums[i];
+			}
+			stack.push(i);
+		}
+		return result;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] nums= {2,1,5,6,2,3};
+		System.out.println(Arrays.toString(nextGreaterElement(nums)));
+		// TODO Auto-generated method stub
+
+	}
+
+}
